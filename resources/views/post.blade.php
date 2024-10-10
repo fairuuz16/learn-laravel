c0b860b4b05dd23ebd2929d07dfe3f2f512d8ab7
fairuuz16 <firuz.firaz@gmail.com>
2024-10-11 04:28:16 +0700

    feat: add eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
       <h2 class="mb-1 text 3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
     </a>
     <div class="text-base text-gray-500">
-      <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+      <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | 1 January 2024
     </div>  
     <p: class="my-4 font-light">
       {{ $post['body'] }}
