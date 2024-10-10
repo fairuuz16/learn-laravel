a3d0b9931ebbc253295db53e1cfbc8fad48daa90
fairuuz16 <firuz.firaz@gmail.com>
2024-10-10 19:25:26 +0700

    feat: add eloquent orm & post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,42 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'John Doe',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, temporibus minus consequatur necessitatibus quas laborum deleniti possimus laudantium ipsum sequi enim eligendi voluptates, quasi asperiores commodi dignissimos nesciunt velit nihil!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'John Doe',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, temporibus minus consequatur necessitatibus quas laborum deleniti possimus laudantium ipsum sequi enim eligendi voluptates, quasi asperiores commodi dignissimos nesciunt velit nihil!'
-            ],
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        // return Arr::first(static::all(), function($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-        
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,8 +15,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog'], ['posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    $post = Post::find($slug);
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
